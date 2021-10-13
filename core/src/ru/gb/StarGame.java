0c1277fbc7b41f0e4ed9491327dca31e8bae0122
phenomiks <phenomikspc@gmail.com>
2021-10-13 19:34:27 +0300

    feat: added background rendering

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -1,6 +1,7 @@
 package ru.gb;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,17 +9,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backgroundImage;
+	private float width;
+	private float height;
 	
 	@Override
 	public void create () {
+		ScreenUtils.getFrameBufferTexture();
 		batch = new SpriteBatch();
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
 		img = new Texture("badlogic.jpg");
+		backgroundImage = new Texture("background.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(backgroundImage, 0, 0, width, height);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
